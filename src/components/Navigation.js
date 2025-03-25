76abe584769e28285b6a1ff96e43b23f90b225b0
sanayamahajan-23 <monagupta9086287092@gmail.com>
2025-03-25 11:59:15 +0530

    added the map

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,64 +1,28 @@
-import React, { useState, useEffect } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import L from "leaflet";
-import axios from "axios";
-import Sidebar from "./Sidebar";
-import SearchBox from "./SearchBox";
-import ChangeView from "./ChangeView";
-import "./Navigation.css"; // Import CSS file
+import React, { useEffect } from "react";
 
-// Fix Leaflet Marker Issue
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-  iconUrl: require("leaflet/dist/images/marker-icon.png"),
-  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
-});
+const MapComponent = () => {
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_GOMAPS_API_KEY; // Get API key from .env
 
-const Navigate = () => {
-  const [position, setPosition] = useState([32.93901, 74.9526]); // Default: SMVDU
-  const [selectedLocation, setSelectedLocation] = useState(null);
+    if (!apiKey) {
+      console.error("GoMaps API key is missing!");
+      return;
+    }
 
-  // Function to update position when a new location is selected
-  const handleLocationSelect = (lat, lon, name) => {
-    setPosition([lat, lon]);
-    setSelectedLocation({ lat, lon, name });
-  };
+    // Initialize GoMaps
+    const script = document.createElement("script");
+    script.src = `https://maps.gomaps.pro/maps/api/js?key=${apiKey}`;
+    script.async = true;
+    script.onload = () => {
+      const map = new window.google.maps.Map(document.getElementById("map"), {
+        center: { lat: 32.94257, lng: 74.95469 }, // SMVDU coordinates
+        zoom: 16,
+      });
+    };
+    document.body.appendChild(script);
+  }, []);
 
-  return (
-    <div className="container">
-      {/* Sidebar */}
-      <Sidebar position={position} />
-
-      {/* Map Container */}
-      <div className="map-container">
-        <SearchBox onLocationSelect={handleLocationSelect} />
-
-        <MapContainer
-          center={position}
-          zoom={15}
-          className="leaflet-map"
-          zoomControl={false}
-        >
-          <ChangeView center={position} />
-          <TileLayer
-            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-            attribution='&copy; <a href="https://carto.com/">CartoDB</a>'
-            subdomains="abcd"
-            maxZoom={20}
-          />
-          <Marker position={position}>
-            <Popup>
-              {selectedLocation ? selectedLocation.name : "You are here!"}{" "}
-              <br />
-              {selectedLocation ? "📍" : "SMVDU, Katra"}
-            </Popup>
-          </Marker>
-        </MapContainer>
-      </div>
-    </div>
-  );
+  return <div id="map" style={{ width: "100%", height: "500px" }} />;
 };
 
-export default Navigate;
+export default MapComponent;
