2e54eca4e755b3e5a0c0d9fbc398ac1d5afa48b3
sanayamahajan-23 <monagupta9086287092@gmail.com>
2025-07-31 02:38:46 +0530

    added the photosphere

--- src/pages/NavigationPage.js
+++ src/pages/NavigationPage.js
@@ -1,3 +1,4 @@
+// Updated NavigationPage.js
 import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import SidePanel from "../components/sidebar";
@@ -6,25 +7,29 @@ import SignIn from "../components/signin";
 import SidePanelPlaceInfo from "../components/SidePanelPlaceInfo";
 import GalleryPanel from "../components/GalleryPanel";
 import DirectionsPanel from "../components/DirectionsPanel";
+import PhotoSphere from "../components/PhotoSphereBasic";
 import { FaDirections } from "react-icons/fa";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 
-// Mapbox Access Token
 mapboxgl.accessToken =
   "pk.eyJ1Ijoic2FuYXlhMTIzIiwiYSI6ImNtZDhpYTh1ZzAwbGsybHNiNjM5MmRwbHYifQ.AP29da_1J7sJ1g4pRP4F9Q";
 
-// Centered coordinates for SMVDU
 const smvduCoords = [74.95410062342953, 32.9422867698961];
 
 const NavigationPage = ({ user }) => {
   const mapContainerRef = useRef(null);
   const mapRef = useRef(null);
+  const markerRef = useRef(null);
+
   const [showDirectionsPanel, setShowDirectionsPanel] = useState(false);
   const [destFromPanel, setDestFromPanel] = useState(null);
   const [selectedPlace, setSelectedPlace] = useState(null);
   const [galleryImages, setGalleryImages] = useState([]);
   const [galleryVisible, setGalleryVisible] = useState(false);
+  const [places, setPlaces] = useState([]);
+  const [photoSpherePlace, setPhotoSpherePlace] = useState(null);
 
-  // Initialize map on mount
   useEffect(() => {
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
@@ -34,25 +39,28 @@ const NavigationPage = ({ user }) => {
     });
 
     mapRef.current.addControl(new mapboxgl.NavigationControl(), "top-right");
-
     return () => mapRef.current.remove();
+  }, []);
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      const querySnapshot = await getDocs(collection(db, "places"));
+      const placeList = [];
+      querySnapshot.forEach((doc) => {
+        placeList.push({ id: doc.id, ...doc.data() });
+      });
+      setPlaces(placeList);
+    };
+    fetchPlaces();
   }, []);
 
   return (
     <div style={{ position: "relative", width: "100%", height: "100vh" }}>
-      {/* Map Container */}
       <div
         ref={mapContainerRef}
-        style={{
-          position: "absolute",
-          top: 0,
-          bottom: 0,
-          right: 0,
-          left: 0,
-        }}
+        style={{ position: "absolute", top: 0, bottom: 0, right: 0, left: 0 }}
       />
 
-      {/* Sidebar */}
       <div
         style={{
           position: "absolute",
@@ -65,22 +73,58 @@ const NavigationPage = ({ user }) => {
         <SidePanel />
       </div>
 
-      {/* SearchBox */}
       <div
-        style={{
-          position: "absolute",
-          top: "20px",
-          left: "80px",
-          zIndex: 30,
-        }}
+        style={{ position: "absolute", top: "20px", left: "80px", zIndex: 30 }}
       >
         {user ? (
           <SearchBox
             mapRef={mapRef}
             user={user}
             onPlaceSelect={(place) => {
-              setSelectedPlace(place);
-              setGalleryVisible(false); // Close gallery on new selection
+              setGalleryVisible(false);
+
+              const matched = places.find(
+                (p) =>
+                  p.id === place.id ||
+                  (p.name === place.name &&
+                    Math.abs(p.lat - place.lat) < 0.0008 &&
+                    Math.abs(p.lng - place.lng) < 0.0008)
+              );
+
+              const enrichedPlace = matched || place;
+              setSelectedPlace(enrichedPlace);
+
+              if (markerRef.current) markerRef.current.remove();
+
+              const el = document.createElement("div");
+              el.className = "mapbox-marker";
+              el.style.cursor = enrichedPlace.photo360
+                ? "pointer"
+                : "not-allowed";
+              el.style.width = "30px";
+              el.style.height = "40px";
+              el.style.backgroundImage =
+                "url('https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png')";
+              el.style.backgroundSize = "100%";
+
+              if (enrichedPlace.photo360) {
+                el.addEventListener("click", () => {
+                  setPhotoSpherePlace(enrichedPlace);
+                });
+              }
+
+              const marker = new mapboxgl.Marker(el)
+                .setLngLat([enrichedPlace.lng, enrichedPlace.lat])
+                .addTo(mapRef.current);
+              markerRef.current = marker;
+
+              mapRef.current.flyTo({
+                center: [enrichedPlace.lng, enrichedPlace.lat],
+                zoom: 17,
+              });
+
+              // do NOT open photosphere immediately
+              setPhotoSpherePlace(null);
             }}
           />
         ) : (
@@ -88,14 +132,8 @@ const NavigationPage = ({ user }) => {
         )}
       </div>
 
-      {/* Floating Directions Button */}
       <div
-        style={{
-          position: "absolute",
-          top: "20px",
-          left: "320px", // adjust depending on search box width
-          zIndex: 30,
-        }}
+        style={{ position: "absolute", top: "20px", left: "320px", zIndex: 30 }}
       >
         <button
           style={{
@@ -116,11 +154,11 @@ const NavigationPage = ({ user }) => {
         </button>
       </div>
 
-      {/* Place Info Panel */}
       {selectedPlace && (
         <SidePanelPlaceInfo
           place={selectedPlace}
           user={user}
+          mapRef={mapRef}
           onGalleryOpen={(photos) => {
             setGalleryImages(photos);
             setGalleryVisible(true);
@@ -133,7 +171,6 @@ const NavigationPage = ({ user }) => {
         />
       )}
 
-      {/* Directions Panel */}
       {showDirectionsPanel && (
         <DirectionsPanel
           mapRef={mapRef}
@@ -142,11 +179,17 @@ const NavigationPage = ({ user }) => {
         />
       )}
 
-      {/* Gallery Panel */}
       {galleryVisible && galleryImages.length > 0 && (
         <GalleryPanel
           photos={galleryImages}
           onClose={() => setGalleryVisible(false)}
+        />
+      )}
+
+      {photoSpherePlace?.photo360 && (
+        <PhotoSphere
+          imageUrl={photoSpherePlace.photo360}
+          onClose={() => setPhotoSpherePlace(null)}
         />
       )}
     </div>
